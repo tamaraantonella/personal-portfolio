cabb5a12150fd0add55763ff568c6cfbbc0f7eaf
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-13 11:09:29 -0300

    init i18next

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import Cta from './Cta'
-import './header.css'
-import ME from '../../assets/me.png'
-import HeaderSocial from './HeaderSocial'
+import React from "react";
+import Cta from "./Cta";
+import "./header.css";
+import ME from "../../assets/me.png";
+import HeaderSocial from "./HeaderSocial";
+import {useTranslation} from "react-i18next";
 
 function Header() {
+  const [t, i18n] = useTranslation("global");
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>{t("header.hello")}</h5>
         <h1>Tamara Antonella Frazzetta</h1>
-        <h5 className="text-light">
-          Fullstack Developer
-        </h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <Cta />
-        <HeaderSocial/>
+        <HeaderSocial />
         <div className="me">
-          <img src={ME} alt=""/>
+          <img src={ME} alt="" />
         </div>
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <button onClick={()=>i18n.changeLanguage("es")}>ES</button>
+        <button onClick={()=>i18n.changeLanguage("en")}>EN</button>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
