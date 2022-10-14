65622969bebbf0d9023bcdf8612075d77e36eace
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-13 21:51:39 -0300

    ok projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,8 @@
-import FOODY from '../../assets/FOODY.png'
-import IMG1 from '../../assets/img1.jpg'
-import IMG2 from '../../assets/img2.jpg'
-import IMG3 from '../../assets/img3.png'
-import React from 'react'
+import FOODY from "../../assets/FOODY.png";
+import IMG1 from "../../assets/img1.jpg";
+import IMG2 from "../../assets/img2.jpg";
+import IMG3 from "../../assets/img3.png";
+import React from "react";
 
 function Projects() {
   return (
@@ -14,24 +14,25 @@ function Projects() {
       <h2 className="text-3xl text-center my-3 md:text-5xl md:text-left mb-5">
         Projects
       </h2>
-      <div className="grid md:grid-cols-3 gap-3">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
         <article className="bg-color_bg_variant border border-solid border-transparent p-[1.4rem] rounded-md transition-all duration-300 flex flex-col w-full hover:border-color_primary_variant hover:bg-transparent">
-          <div className="portfolio__item-image">
+          <div className="rounded-md w-full h-[200px] md:h-[250px]">
             <img
+              className="w-full h-full object-cover object-center transform ease-in-out duration-300 hover:scale-[1.01] rounded-md"
               src="https://res.cloudinary.com/dfbxjt69z/image/upload/v1665519582/Screenshot_2022-10-11_at_13-14-34_Mascotapp_xeqn4k.png"
               alt=""
             />
           </div>
-          <h3>Mascotapp</h3>
-          <h5>SPA Bootcamp SoyHenry</h5>
-          <h5>
+          <h3 className="font-semibold text-xl mt-3">Mascotapp</h3>
+          <h5 className="text-color_light">SPA Bootcamp SoyHenry</h5>
+          <h5 className="text-color_light mb-3">
             Tech: Js, Ts, Node.js, ReactJs, Redux, Express, PostgreSQL, Auth0,
             Stripe, Tailwind and Material UI.
           </h5>
-          <div className="portfolio__item-cta">
+          <div className="flex gap-2 mb-3">
             <a
               href="https://github.com/laureanomarenco/mascotapps-front"
-              className="btn"
+              className="border border-solid rounded-md border-color_primary text-color_primary  px-5 py-2 w-max-content block pointer hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -39,7 +40,7 @@ function Projects() {
             </a>
             <a
               href="https://mascotapps.vercel.app/"
-              className="btn btn-primary"
+              className="border border-solid rounded-md bg-color_primary px-5 py-2 w-max-content block pointer text-color_bg_variant  hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -48,19 +49,25 @@ function Projects() {
           </div>
         </article>
         <article className="bg-color_bg_variant border border-solid border-transparent p-[1.4rem] rounded-md transition-all duration-300 flex flex-col w-full hover:border-color_primary_variant hover:bg-transparent">
-          <div className="portfolio__item-image">
-            <img src={FOODY} alt="" />
+          <div className="rounded-md w-full h-[200px] md:h-[250px]">
+            <img
+              className="w-full h-full object-cover object-center transform ease-in-out duration-300 hover:scale-[1.01] rounded-md"
+              src={FOODY}
+              alt=""
+            />
           </div>
-          <h3>PI-Foody </h3>
-          <h5>SPA - recipes -- Bootcamp SoyHenry</h5>
-          <h5>
+          <h3 className="font-semibold text-xl mt-3">PI-Foody </h3>
+          <h5 className="text-color_light">
+            SPA - recipes -- Bootcamp SoyHenry
+          </h5>
+          <h5 className="text-color_light mb-3">
             Tech: Javascript, React, Redux, NodeJS, Express, HTML5, CSS3,
             Sequelize y PostgreSQL
           </h5>
-          <div className="portfolio__item-cta">
+          <div className="flex gap-2 mb-3">
             <a
               href="https://github.com/tamaraantonella/PI-food-tamara"
-              className="btn"
+              className="border border-solid rounded-md border-color_primary text-color_primary  px-5 py-2 w-max-content block pointer hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -68,7 +75,7 @@ function Projects() {
             </a>
             <a
               href="https://foody-app-indol.vercel.app/"
-              className="btn btn-primary"
+              className="border border-solid rounded-md bg-color_primary px-5 py-2 w-max-content block pointer text-color_bg_variant  hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -77,16 +84,22 @@ function Projects() {
           </div>
         </article>
         <article className="bg-color_bg_variant border border-solid border-transparent p-[1.4rem] rounded-md transition-all duration-300 flex flex-col w-full hover:border-color_primary_variant hover:bg-transparent">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
+          <div className="rounded-md w-full h-[200px] md:h-[250px]">
+            <img
+              className="w-full h-full object-cover object-center transform ease-in-out duration-300 hover:scale-[1.01] rounded-md"
+              src={IMG1}
+              alt=""
+            />
           </div>
-          <h3>Huerta del tomate website</h3>
-          <h5>Website HTML, CSS, SASS -- Coderhouse</h5>
+          <h3 className="font-semibold text-xl mt-3">Huerta del tomate website</h3>
+          <h5 className="text-color_light mb-3">
+            Website HTML, CSS, SASS -- Coderhouse
+          </h5>
 
-          <div className="portfolio__item-cta">
+          <div className="flex gap-2 mb-3">
             <a
               href="https://github.com/tamaraantonella/pf-huerta-del-tomate"
-              className="btn"
+              className="border border-solid rounded-md border-color_primary text-color_primary  px-5 py-2 w-max-content block pointer hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -94,7 +107,7 @@ function Projects() {
             </a>
             <a
               href="https://huertadeltomate.netlify.app/"
-              className="btn btn-primary"
+              className="border border-solid rounded-md bg-color_primary px-5 py-2 w-max-content block pointer text-color_bg_variant  hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -103,15 +116,21 @@ function Projects() {
           </div>
         </article>
         <article className="bg-color_bg_variant border border-solid border-transparent p-[1.4rem] rounded-md transition-all duration-300 flex flex-col w-full hover:border-color_primary_variant hover:bg-transparent">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
+          <div className="rounded-md w-full h-[200px] md:h-[250px]">
+            <img
+              className="w-full h-full object-cover object-center transform ease-in-out duration-300 hover:scale-[1.01] rounded-md"
+              src={IMG2}
+              alt=""
+            />
           </div>
-          <h3>Ecommerce shoe box</h3>
-          <h5>Website HTML, CSS & Js --Coderhouse</h5>
-          <div className="portfolio__item-cta">
+          <h3 className="font-semibold text-xl mt-3">Ecommerce shoe box</h3>
+          <h5 className="text-color_light mb-3">
+            Website HTML, CSS & Js --Coderhouse
+          </h5>
+          <div className="flex gap-2 mb-3">
             <a
               href="https://github.com/tamaraantonella/js_coder_house"
-              className="btn"
+              className="border border-solid rounded-md border-color_primary text-color_primary  px-5 py-2 w-max-content block pointer hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -119,7 +138,7 @@ function Projects() {
             </a>
             <a
               href="https://tamaraantonella.github.io/js_coder_house/"
-              className="btn btn-primary"
+              className="border border-solid rounded-md bg-color_primary px-5 py-2 w-max-content block pointer text-color_bg_variant  hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -128,15 +147,19 @@ function Projects() {
           </div>
         </article>
         <article className="bg-color_bg_variant border border-solid border-transparent p-[1.4rem] rounded-md transition-all duration-300 flex flex-col w-full hover:border-color_primary_variant hover:bg-transparent">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
+          <div className="rounded-md w-full h-[200px] md:h-[250px]">
+            <img
+              className="w-full h-full object-cover object-center transform ease-in-out duration-300 hover:scale-[1.01] rounded-md"
+              src={IMG3}
+              alt=""
+            />
           </div>
-          <h3>LP Hollistic</h3>
-          <h5>Landing page - HTML & CSS</h5>
-          <div className="portfolio__item-cta">
+          <h3 className="font-semibold text-xl mt-3">LP Hollistic</h3>
+          <h5 className="text-color_light mb-3">Landing page - HTML & CSS</h5>
+          <div className="flex gap-2 mb-3">
             <a
               href="https://github.com/tamaraantonella/adriana_amorelli_holistic"
-              className="btn"
+              className="border border-solid rounded-md border-color_primary text-color_primary  px-5 py-2 w-max-content block pointer hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -144,7 +167,7 @@ function Projects() {
             </a>
             <a
               href="https://tamaraantonella.github.io/adriana_amorelli_holistic/"
-              className="btn btn-primary"
+              className="border border-solid rounded-md bg-color_primary px-5 py-2 w-max-content block pointer text-color_bg_variant  hover:bg-color_white transition ease-in-out duration-300 hover:text-color_bg hover:border-transparent"
               target="_blank"
               rel="noreferrer"
             >
@@ -157,4 +180,4 @@ function Projects() {
   );
 }
 
-export default Projects
+export default Projects;
