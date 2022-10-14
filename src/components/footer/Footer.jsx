4ef7ea66dba8630db006bc07695bd99a0d07794f
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-13 22:27:40 -0300

    ok remake

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,14 @@
-import React from 'react'
-import './footer.css'
-import { AiOutlineGithub } from 'react-icons/ai'
-import {IoLogoLinkedin} from 'react-icons/io'
-import { AiFillDribbbleCircle } from 'react-icons/ai'
-import { AiOutlineTwitter } from 'react-icons/ai'
+import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <ul className="permalinks">
-        <li><a href="#top">Home</a></li>
-        <li><a href="about">About me</a></li>
-        <li><a href="#experience">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/tamara-frazzetta/" target="_blank" rel="noreferrer"> <IoLogoLinkedin/> </a>
-        <a href="https://github.com/tamaraantonella" target="_blank" rel="noreferrer"> <AiOutlineGithub/> </a>
-        <a href="https://dribbble.com/tamaraf" target="_blank" rel="noreferrer"> <AiFillDribbbleCircle/> </a>
-        <a href="https://twitter.com/TamaraaDev" target="_blank" rel="noreferrer"> <AiOutlineTwitter/> </a>
-      </div>
-      <div className="footer__copyright">
-        <small>&copy; Tamara Antonella Frazzetta. All rights reserved</small>
+    <footer className="bg-color_primary py-3 pt-16 text-center ">
+      <div className="flex gap-5 justify-center">
+        <small>&copy; 2022 Tamara Antonella Frazzetta. </small>
+        <small>All rights reserved.</small>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
