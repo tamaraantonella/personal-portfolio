9921f9724c60a7cf9538b0277ae943733732b382
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-17 13:34:53 -0300

    express icon

--- src/components/experience/IconLink.jsx
+++ src/components/experience/IconLink.jsx
@@ -6,7 +6,7 @@ export default function IconLink({ path, icon, name }) {
       href={path}
       target="_blank"
       rel="noreferrer"
-      className=" flex justify-center pb-3 transition-all ease-in-out duration-500 md:grayscale hover:grayscale-0 cursor-pointer "
+      className={`flex justify-center pb-3 transition-all ease-in-out duration-500 md:grayscale hover:grayscale-0 cursor-pointer ${name === 'express' && 'invert'}`}
     >
       <img src={icon} alt={name} />
     </a>
