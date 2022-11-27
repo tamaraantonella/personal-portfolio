24fc6ddb217b0acd026d296e9cd856baacb2cd34
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-27 19:04:06 -0300

    fix App imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { About, Header, Experience, Projects, Contact, Footer, Nav, ParticlesBg, LangSwitch } from "./components";
+import { About, Header, Experience, Projects, Contact, Footer, Nav, ParticlesBg, } from "./components";
 import { motion, useScroll, useSpring } from "framer-motion";
 import { container, easing, stagger, subtitleAndContent, title, transition } from "./constants/animation";
 
@@ -15,17 +15,18 @@ const App = () => {
 
   return (
     <>
-    <ParticlesBg />
-    <motion.div className="sm:mx-auto box-border">
-      <motion.div style={{ scaleX }} className="progress-bar" />
-      <Nav stagger={stagger} easing={easing} />
-      <Header stagger={stagger} easing={easing} transition={transition} />
-      <About container={container} subtitleAndContent={subtitleAndContent} title={title} />
-      <Experience container={container} subtitleAndContent={subtitleAndContent} title={title} />
-      <Projects container={container} subtitleAndContent={subtitleAndContent} title={title} />
+      <ParticlesBg />
+      <motion.div className="sm:mx-auto box-border">
+        <motion.div style={{ scaleX }} className="progress-bar" />
+
+        <Nav stagger={stagger} easing={easing} />
+        <Header stagger={stagger} easing={easing} transition={transition} />
+        <About container={container} subtitleAndContent={subtitleAndContent} title={title} />
+        <Experience container={container} subtitleAndContent={subtitleAndContent} title={title} />
+        <Projects container={container} subtitleAndContent={subtitleAndContent} title={title} />
         <Contact container={container} subtitleAndContent={subtitleAndContent} title={title} />
-      <Footer />
-    </motion.div>
+        <Footer />
+      </motion.div>
     </>
   );
 };
