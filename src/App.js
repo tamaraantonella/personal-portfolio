bebd6d7e183de975a54b50b5e21e41f6e19f07ab
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-27 14:02:17 -0300

    fix: Add motion frame constants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { About, Header, Experience, Projects, Contact, Footer, Nav, LangSwitch } from "./components";
 import { motion, useScroll, useSpring } from "framer-motion";
+import { easing, stagger, transition } from "./constants/animation";
 
 
 
@@ -11,23 +12,6 @@ const App = () => {
     damping: 30,
     restDelta: 0.001
   });
-  let easing = [0.6, -0.05, 0.01, 0.99];
-
-  const stagger = {
-    animate: {
-      transition: {
-        staggerChildren: 0.2,
-        delayChildren: 0.3,
-        staggerDirection: 1,
-      },
-    },
-  };
-
-  const transition = { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] };
-
-
-
-
 
   return (
     <motion.div className="mx-auto relative">
---
+++ src/constants/animation.js
@@ -0,0 +1,13 @@
+export const easing = [0.6, -0.05, 0.01, 0.99];
+
+export const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.3,
+      staggerDirection: 1,
+    },
+  },
+};
+
+export const transition = { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] };
